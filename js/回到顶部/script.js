baaa728080cade9a27eadaf9d8f125e314f51a1f
xcl <x18752457230@gmail.com>
2019-04-04 12:29:09 +0800

    回到顶部

---
+++ js/回到顶部/script.js
@@ -0,0 +1,21 @@
+const CD_TOP_SELECTOR = '.cd-top';
+const header = document.querySelector('header');
+const b = document.querySelector('body');
+const h = document.querySelector('html');
+const cdTop = document.querySelector(CD_TOP_SELECTOR);
+
+cdTop.onclick = () => {
+  $('html').animate({ scrollTop: 0 }, 500);
+};
+
+b.onscroll = () => {
+	// if(b.scrollTop === 30) {
+		// console.log(document.querySelector('html').scrollTop);
+	// }
+  if (h.scrollTop >= 100){
+    console.log(h.scrollTop);
+    header.classList.add('head-min');
+  } else {
+    header.classList.remove('head-min');
+  }
+};
